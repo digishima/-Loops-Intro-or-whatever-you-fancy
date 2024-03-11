f703b3f0cb4ab7461f1b0bf7ca190a407f9a8fa6
Michel Thompson <geekgreekgod@gmail.com>
2024-03-11 03:23:40 -0400

    Loop exercises completed because I am a rockstar

---
+++ main.js
@@ -0,0 +1,11 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+
+for (var i = 0; i < cohortOneStudents.length; i++) {
+    console.log(cohortOneStudents[i]);
+}
+
+const interests = ["Reading", "Cooking", "Cosplay", "Drawing", "Writing"];
+
+for (let i = 0; i < interests.length; i++) {
+    console.log(`One of my interests is: ${interests[i]}.`);
+}
